af0e17944d15f0bb166deb58c1f8c7cda86b01ab
Jojoksv <124268194+Jojoksv@users.noreply.github.com>
2024-02-23 18:59:04 +0000

    error Page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,15 +1,19 @@
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import '../styles/Error.css'
 
 function Error() {
     const navigateTo = useNavigate();
+    const location = useLocation();
 
-  const handleGoBack = () => {
-    navigateTo(-1);
-  };
+    const handleGoBack = () => {
+        const previousPage = location.state?.from || '/';
+        navigateTo(previousPage);
+    };
+
 
   return (
-    <div>
+    <div className='errorcss'>
       <h1>Oups... Page non trouvée</h1>
       <p>Il semblerait que la page que vous cherchez n’existe pas.</p>
       <button onClick={handleGoBack}>Revenir en arrière</button>
