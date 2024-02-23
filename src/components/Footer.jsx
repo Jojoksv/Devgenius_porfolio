fe9d87fdd30a1aa9fb525dca814021cef37c3f8a
Joseph Kossouvi <124268194+Jojoksv@users.noreply.github.com>
2024-02-23 13:37:24 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ import '../styles/Footer.css'
 function Footer(){
     return(
         <footer>
-            <div className="footerFooter">
-                <div className='footerTitle'>
+            <div className="footerFooter projetdevgenius joseph Kossouvi jojoksv devgenius">
+                <div className='footerTitle projetdevgenius joseph Kossouvi jojoksv devgenius'>
                     <div className="footerdoted"></div>
                     <h1>DEV<span>GENIUS</span></h1>
                 </div>
-                <div className='AboutMe'>
+                <div className='AboutMe projetdevgenius joseph Kossouvi jojoksv devgenius'>
                     <span>BURKINA FASO, Ouagadougou</span>
                     <a href="tel:+22656146917">Tel: +226 56146917</a>
                     <a href="mailto:jkossouvi@gmail.com">jkossouvi@gmail.com</a>
